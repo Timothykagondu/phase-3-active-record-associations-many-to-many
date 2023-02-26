da6fee2c9a6dde1ee65db4f730fd66a76c28a691
TimothyKagondu <timothy.2k21@gmail.com>
2023-02-26 19:30:10 +0300

    completed assignment

--- db/schema.rb
+++ db/schema.rb
@@ -29,4 +29,10 @@ ActiveRecord::Schema.define(version: 2021_07_19_113221) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+  
 end
